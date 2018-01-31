68ee799ec3ffdffce691e1559779770af9ed8483
Oksana Korol <okorol@gmail.com>
2018-01-31 16:12:28 -0500

    Changed the structure of DNA_Barcode dir to reflect discussion with Wen, Izhar, Pen

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import os
 
 start_dir = "out/reference"
 ncbi_wholegenomes_kingdoms = ["archaea", "bacteria", "fungi", "invertebrate"]
-barcode_dirs = ["16S_Bacteria", "16S_Archea", "Fungi_ITS", "Insect_CO1"]
+#barcode_dirs = ["16S_Bacteria", "16S_Archea", "Fungi_ITS", "Insect_CO1"]
+barcode_db_dirs = ["16S_Bacteria/GreenGenes", "16S_Archea/GreenGenes", "Fungi_ITS/Unite", "Insect_CO1/ncbi"]
 # Move the taxonomy out to the same level as WholeGenome
 #ref_data_dirs = ['Taxonomy', 'Fasta', 'ToolDB']
-ref_data_dirs = ['Fasta', 'ToolDB']
+whole_genome_db_dirs = ['Fasta', 'ToolDB']
+barcodes_db_subdirs = ['Fasta', 'Taxonomy']
 taxonomy_formats = ['qiime', 'mothur']
 
 # NCBI
@@ -24,36 +26,38 @@ ncbi_assembly_summary_file = "assembly_summary.txt"
 
 def main():
     try:
+        ### Whole Genome
         for k in ncbi_wholegenomes_kingdoms:
             kingdom_dir = "{0}/WholeGenome/{1}".format(start_dir, k)
-            for d in ref_data_dirs:
+            for d in whole_genome_db_dirs:
                 os.makedirs("{0}/ncbi/RefSeq/{1}".format(kingdom_dir, d), exist_ok=True)
                 os.makedirs("{0}/ncbi/GenBank/{1}".format(kingdom_dir, d), exist_ok=True)
             os.makedirs("{0}/WholeGenome/ToolDB/Humann".format(start_dir), exist_ok=True)
             os.makedirs("{0}/WholeGenome/ToolDB/Metamos".format(start_dir), exist_ok=True)
             os.makedirs("{0}/WholeGenome/ToolDB/Kraken".format(start_dir), exist_ok=True)
 
-
-        for k in barcode_dirs:
+        ### Barcodes
+        for k in barcode_db_dirs:
             barcode_dir = "{0}/DNA_Barcodes/{1}".format(start_dir, k)
-            for d in ref_data_dirs:
-                # This is probably too specific for barcodes
-                # os.makedirs("{0}/ncbi/RefSeq/{1}".format(loci_dir, d), exist_ok=True)
-                # os.makedirs("{0}/ncbi/GenBank/{1}".format(loci_dir, d), exist_ok=True)
-                os.makedirs("{0}/ncbi/{1}".format(barcode_dir, d), exist_ok=True)
+            for d in barcodes_db_subdirs:
+                os.makedirs("{0}/{1}".format(barcode_dir, d), exist_ok=True)
 
+
+        ### Taxonomy
         for k in taxonomy_formats:
             taxonomy_dir = "{0}/Taxonomy/ncbi/ToolFormats/{1}".format(start_dir, k)
             os.makedirs(taxonomy_dir, exist_ok=True)
 
-        # ncbi_nr_nt is not a very good name, think of a better one
-        os.makedirs("{0}/ncbi_nr_nt".format(start_dir), exist_ok=True)
-
+        ### Protein
         # If we want proteins in the future, they should be at this level:
         #os.makedirs("{0}/Protein".format(start_dir), exist_ok=True)
         # refseq_protein database
         os.makedirs("{0}/Protein/ncbi/RefSeq/".format(start_dir), exist_ok=True)
         os.makedirs("{0}/Protein/ToolDB/Humann/".format(start_dir), exist_ok=True)
+
+        ### Everything
+        # ncbi_nr_nt is not a very good name, think of a better one
+        os.makedirs("{0}/ncbi_nr_nt".format(start_dir), exist_ok=True)
 
     except OSError as e:
         if e.errno != errno.EEXIST:
